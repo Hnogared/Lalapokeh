289c5434f6504070f0715b4b3a46d0bb1fec4e00
Lara Schmid <lara.s.schmid@icloud.com>
2025-05-08 16:54:40 +0200

    style: added cutsom theme (#4)

--- Lalapokeh/Program.cs
+++ Lalapokeh/Program.cs
@@ -1,11 +1,18 @@
 using Lalapokeh;
+using Lalapokeh.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices();
+
+builder.Services.AddSingleton<AppThemeService>();
 
 await builder.Build().RunAsync();
