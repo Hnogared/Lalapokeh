eb3e548614a1f5b5401d1be1dc97a1aef2e10ff1
Schmid <lara.schmid@cgi.com>
2025-05-09 08:57:14 +0200

    chore: format files

--- Lalapokeh/Services/AppThemeService.cs
+++ Lalapokeh/Services/AppThemeService.cs
@@ -2,25 +2,25 @@
 
 namespace Lalapokeh.Services
 {
-	public class AppThemeService
-	{
-		public Dictionary<string, string> CustomColors { get; set; } = new()
-		{
-			{ "Primary", "#4285F4" },
-			{ "Secondary", "#FFCB05" },
-			{"Tertiay", "#828282" },
-			{" ActionDefault", "#000000" },
-			{"Water", "#4D90D5"},
-			{"Grass", "#61BB5B" },
-			{"Fire", "#FFB167" },
-			{"Poisen", "#AB6AC8" },
-			{"Ground" , "#DD4715"},
-			{"Fairy", "#FFA4FD" },
-			{"Normal", "#9C9C9C" },
-			{"Electric", "#F3D23B"},
-			{"Not Found", "#EB4335" },
-			{"Go back to home", "#EB4335"},
-		};
-       
-	}
+  public class AppThemeService
+  {
+    public Dictionary<string, string> CustomColors { get; set; } = new()
+    {
+      { "Primary", "#4285F4" },
+      { "Secondary", "#FFCB05" },
+      {"Tertiay", "#828282" },
+      {" ActionDefault", "#000000" },
+      {"Water", "#4D90D5"},
+      {"Grass", "#61BB5B" },
+      {"Fire", "#FFB167" },
+      {"Poisen", "#AB6AC8" },
+      {"Ground" , "#DD4715"},
+      {"Fairy", "#FFA4FD" },
+      {"Normal", "#9C9C9C" },
+      {"Electric", "#F3D23B"},
+      {"Not Found", "#EB4335" },
+      {"Go back to home", "#EB4335"},
+    };
+
+  }
 }
