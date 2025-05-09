e20e1a7fc9c653e5a24db3b56ee43cc38e3bdf26
Hnogared <133124217+Hnogared@users.noreply.github.com>
2025-05-09 10:10:10 +0200

    feat: main layout

--- Lalapokeh/Models/API/Common/NamedApiResource.cs
+++ Lalapokeh/Models/API/Common/NamedApiResource.cs
@@ -1,13 +1,6 @@
 ﻿namespace Lalapokeh.Models.API.Common
 {
-<<<<<<< HEAD
   public class NamedApiResource
   {
   }
 }
-=======
-	public class NamedApiResource
-	{
-	}
-}
->>>>>>> 6c02dd18d0a6e7454cb9c4ae3bd349f7fe64dff3
--- Lalapokeh/Models/API/Pokemon.cs
+++ Lalapokeh/Models/API/Pokemon.cs
@@ -1,13 +1,6 @@
 ﻿namespace Lalapokeh.Models.API
 {
-<<<<<<< HEAD
   public class Pokemon
   {
   }
 }
-=======
-	public class Pokemon
-	{
-	}
-}
->>>>>>> 6c02dd18d0a6e7454cb9c4ae3bd349f7fe64dff3
