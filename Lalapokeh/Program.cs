e8c4b56575c40b3f517cefbd3fe0bbdef44c8928
Hnogared <133124217+Hnogared@users.noreply.github.com>
2025-05-11 16:02:00 +0200

    chore: added appsettings.json

--- Lalapokeh/Program.cs
+++ Lalapokeh/Program.cs
@@ -5,13 +5,43 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+#region Root components
+builder.RootComponents.Add<App>("#app");
+
+builder.RootComponents.Add<HeadOutlet>("head::after");
+#endregion
+
+#region Services
+builder.Services.AddScoped(sp =>
+{
+  var baseUrl = builder.Configuration["ApiUrl"];
+
+  if (baseUrl is null)
+  {
+    throw new InvalidOperationException("API URL must be set in the app settings.");
+  }
+
+  // Ensure HTTPS is enforced
+  if (!baseUrl.StartsWith("https", StringComparison.OrdinalIgnoreCase))
+  {
+    throw new InvalidOperationException("API must be accessed over HTTPS.");
+  }
+
+  return new HttpClient
+  {
+    BaseAddress = new Uri(baseUrl),
+    Timeout = TimeSpan.FromSeconds(
+      int.TryParse(builder.Configuration["RequestTimeout"], out var timeout) ? timeout : 10
+    )
+  };
+});
 
 builder.Services.AddMudServices();
 
 builder.Services.AddSingleton<AppThemeService>();
+
+builder.Services.AddScoped<PokemonService>();
+#endregion
 
 await builder.Build().RunAsync();
