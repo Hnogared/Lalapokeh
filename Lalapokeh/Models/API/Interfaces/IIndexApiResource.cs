73bc96e63f8011c5a931d28a4532e42efbc8a33b
Hnogared <133124217+Hnogared@users.noreply.github.com>
2025-05-14 09:43:25 +0200

    feat: app cards (#57)
    * build: upgraded .net version + added required mudblazor js
    
    * chore: added type icons and css classes
    
    * chore: extended BasePokeApiService
    added error management
    added logging
    implemented caching
    
    * feat: api data for the pokemon overview
    
    * build: updated workflows to the new dotnet version

---
+++ Lalapokeh/Models/API/Interfaces/IIndexApiResource.cs
@@ -0,0 +1,10 @@
+﻿namespace Lalapokeh.Models.API.Interfaces
+{
+  /// <summary>
+  /// Interface for the models implementing an ID.
+  /// </summary>
+  public interface IIndexApiResource
+  {
+    int Id { get; }
+  }
+}
--- Lalapokeh/Services/PokemonService.cs
+++ Lalapokeh/Services/PokemonService.cs
@@ -1,10 +1,16 @@
 ﻿using Lalapokeh.Models.API.Pokemon;
 using Lalapokeh.Services.Abstractions;
+using System.Text.Json;
 
 namespace Lalapokeh.Services
 {
   /// <summary>
   /// Service for retrieving Pokémon data from the PokeAPI.
   /// </summary>
-  public class PokemonService(HttpClient httpClient) : BasePokeApiService<Pokemon>(httpClient, "pokemon") { }
+  public class PokemonService(
+    HttpClient httpClient,
+    ILogger<PokemonService> logger,
+    JsonSerializerOptions jsonOptions
+  ) : BasePokeApiService<Pokemon>(httpClient, "pokemon", logger, jsonOptions)
+  { }
 }
